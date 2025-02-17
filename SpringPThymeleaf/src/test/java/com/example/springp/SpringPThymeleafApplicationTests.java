e227e1a26f257d299a3d8ed70197cb93d500b917
KWAK RAEHUI <glasofk12345@naver.com>
2025-02-17 18:19:27 +0900

    나머지 올리기

---
+++ SpringPThymeleaf/src/test/java/com/example/springp/SpringPThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
