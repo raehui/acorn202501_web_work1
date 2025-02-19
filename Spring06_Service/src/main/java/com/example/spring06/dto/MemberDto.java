fb8645c33327b0288a2dd4b59236f6285a1482cf
KWAK RAEHUI <glasofk12345@naver.com>
2025-02-19 17:08:13 +0900

    spring06 프로젝트 alias 올림

--- Spring06_Service/src/main/java/com/example/spring06/dto/MemberDto.java
+++ Spring06_Service/src/main/java/com/example/spring06/dto/MemberDto.java
@@ -1,9 +1,14 @@
 package com.example.spring06.dto;
 
+import org.apache.ibatis.type.Alias;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
+/*
+ * type 에 별칭을 부여하고 Mapper xml 문서에서 별칭을 이용해서 parameterType 과 resultType 설정을 할 수 잇다.
+ */
+@Alias("memberDto")
 @Getter
 @Setter
 public class MemberDto {
