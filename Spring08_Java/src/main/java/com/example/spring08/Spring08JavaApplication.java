e0c2bf444f459c0a702f1c6225febb5615c28179
KWAK RAEHUI <glasofk12345@naver.com>
2025-02-20 15:52:08 +0900

    sprign08_java 예제 올리기

---
+++ Spring08_Java/src/main/java/com/example/spring08/Spring08JavaApplication.java
@@ -0,0 +1,68 @@
+package com.example.spring08;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring08JavaApplication.class, args);
+		//of() 메소드로 만든 List 는 읽기전용(read only)이다.
+		List<String> names=List.of("김구라","원숭이","해골");
+		//names.add("주뎅이"); //동작하지 않는다.(예외발생)
+		
+		//List의 stream()의 메소드를 호출하면 Stream type 이 리턴된다.
+		Stream<String> stream=names.stream();
+		
+		Function<String, String> f= (item) -> {
+			return item+"님";
+		};
+		//위의 Function type 을 줄여서 쓰면 아래와 같다.
+		Function<String, String> f2=item -> item+"님";
+		
+		//stream에서 map 사용은 단 한번만 실행 가능
+		List<String> names2=stream.map(f).toList();
+		System.out.println(names2);
+		
+		//단 한줄의 코딩으로 새로운 리스트 생성함
+		//stream은 map 메서드를 사용하기 위해서 작성
+		//리스트를 stream 타입으로 만들고 map 메서드를 사용함
+		List<String> names3=names.stream().map(f2).toList();
+		System.out.println(names3);
+		
+		List<String> names4=names.stream().map(item->item +"님").toList();
+		
+		List<Integer> nums = List.of(-10, 20, 30, -5, 25, -30);
+		//양의 정수만 남긴 리스트
+		List<Integer> newNums=nums.stream().filter(item -> item>0).toList();
+		System.out.println(newNums);
+		//양의 정수 2배
+		List<Integer> newNums2=nums.stream().filter(item -> item>0).map(item->item*2).toList();
+		System.out.println(newNums2);
+		
+		//nums 에서 양의 정수만 남기고 2배를 곱한 새로운 List 얻어내서 순서대로 해당 숫자를 콘솔창에 모두 출력하기
+		nums.stream().filter(item->item>0).map(item->item*2).forEach(item->{
+			System.out.println(item);
+		});
+		//문자열(String)이 들어 있는 List
+		List<String> strNums= List.of("10","20","30","40","50");
+		
+		//위의 List를 활용해서 List<Integer>로 얻어내 보세요.
+		List<Integer> intNums=strNums.stream().map(item->Integer.parseInt(item)).toList();
+		System.out.println(intNums);
+		// Integer 클래스가 가지고 있는 parseInt 메소드를 참조해서 map()함수에 전달해서 동일한 작업도 가능하다.
+		List<Integer> intNums2=strNums.stream().map(Integer::parseInt).toList();
+		System.out.println(intNums2);
+		
+		
+
+	}
+	
+	
+
+}
---
+++ SpringPThymeleaf/src/main/java/com/example/springp/controller/FileController.java
@@ -0,0 +1,168 @@
+package com.example.springp.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.springp.dto.FileDto;
+
+@Controller
+public class FileController {
+	/* custom.properties 파일이 로딩 되기 위해서는 main 메소드가 
+	 * 있는 클래스에 @PropertySource 어노테이션 설정이 되어 있어야 한다.
+	 * 
+	 * custom.properties 파일에 있는 파일 업로드
+	 * 경로를 읽어와서 필드에 담는다.
+	 */
+	
+	@Value("${file.location}")
+	private String fileLocation;
+	
+	/*
+	 * ResponseEntity<InputStreamResource> 는 파일을 다운로드 할때 사용하는 리턴 type 이다.
+	 */
+	@GetMapping("/file/download")
+	public ResponseEntity<InputStreamResource> download(String orgFileName, String saveFileName, long fileSize){
+		
+		//원래는 DB 에서 읽어와야 하지만 지금은 다운로드해줄 파일의 정보가 요청 파라미터로 전달된다.
+		try {
+			//다운로드 시켜줄 원본 파일명 인코딩한다.
+			String encodedName=URLEncoder.encode(orgFileName, "utf-8");
+			//파일명에 공백이 있는경우 파일명이 이상해지는걸 방지
+			encodedName=encodedName.replaceAll("\\+"," ");
+			//응답 헤더정보(스프링 프레임워크에서 제공해주는 클래스) 구성하기 (웹브라우저에 알릴정보)
+			HttpHeaders headers=new HttpHeaders();
+			//파일을 다운로드 시켜 주겠다는 정보
+			headers.add(HttpHeaders.CONTENT_TYPE, "application/octet-stream"); 
+			//파일의 이름 정보(웹브라우저가 해당정보를 이용해서 파일을 만들어 준다)
+			headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename="+encodedName);
+			//파일의 크기 정보도 담아준다.
+			headers.setContentLength(fileSize);
+			
+			//읽어들일 파일의 경로 구성
+			//다운로드할 파일의 경로를 얻기위해서 작성
+			String filePath=fileLocation + File.separator + saveFileName;
+			
+			/*
+			 * 다운로드할 인코딩된 원본 파일명, 빨대를 꼽을 저장된 파일명
+			 */
+			
+			//파일에서 읽어들일 스트림 객체
+			//다운로드 원하는 파일에 빨대를 꼽기위해서 작성했다.
+			InputStream is=new FileInputStream(filePath);
+			//InputStreamResource 객체의 참조값 얻어내기
+			InputStreamResource isr=new InputStreamResource(is);
+			//ResponseEntity 객체를 구성해서
+			ResponseEntity<InputStreamResource> resEntity=ResponseEntity.ok()
+					.headers(headers)
+					.body(isr);
+			//리턴해주면서 파일이 다운로드 된다.
+			return resEntity;
+		}catch(Exception e) {
+			//예외 정보를 콘솔에 출력
+			e.printStackTrace();
+			//에외 발생시키기
+			throw new RuntimeException("파일을 다운로드 하는중에 에러 발생!");
+			
+		}
+		
+	}
+	
+	/*
+	 * <input type="file" name="myFile">
+	 * 
+	 * 은 MultipartFile myFile 로 해서 업로드된 파일의 정보를 얻어내야 한다.
+	 * spring이 자동으로 임시파일에 저장한다. = 서버의 파일 시스템 어딘가에 저장한다...(c:에 있는 어딘가의 파일에...)
+	 * 그러면 우리가 원하는 파일로 옮기고 DB에 저장할 수도 있다.
+	 * 
+	 * MultipartFile은 파일을 처리하는 객체
+	 * 
+	 */
+	@PostMapping("/file/upload")
+	public String upload(String title, MultipartFile myFile, Model model) {		
+		//만일 파일이 업로드 되지 않았다면
+		if(myFile.isEmpty()) {
+			throw new RuntimeException("파일이 업로드 되지 않았습니다.");
+		}
+		
+		//원본파일명
+		String orgFileName=myFile.getOriginalFilename();
+		//파일의 크기
+		long fileSize=myFile.getSize();
+		//저장할 파일의 이름을 Universal Unique 한 문자열로 얻어내기
+		String saveFileName=UUID.randomUUID().toString();
+		//저장할 파일의 전체 경로 구성하기
+		String filePath=fileLocation+File.separator+saveFileName;
+		try {
+			//업로드 파일을 저장할 파일 객체 생성
+			File savefile=new File(filePath);
+			myFile.transferTo(savefile);
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//원래는 DB에 저장해야 하지만 테스트를 위해 view page 에 전달해서 출력한다.
+		model.addAttribute("title",title);
+		model.addAttribute("orgFileName",orgFileName);
+		model.addAttribute("fileSize",fileSize);
+		model.addAttribute("saveFileName",saveFileName);
+		
+		return "file/upload";
+	}
+	
+	@PostMapping("/file/upload2")
+	public String upload2(FileDto dto,Model model) {		
+		// FileDto 객체에서 MultipartFile 객체를 얻어낸다.
+		MultipartFile myFile=dto.getMyFile();
+		
+		//만일 파일이 업로드 되지 않았다면
+		if(myFile.isEmpty()) {
+			throw new RuntimeException("파일이 업로드 되지 않았습니다.");
+		}
+		
+		//원본파일명
+		String orgFileName=myFile.getOriginalFilename();
+		//파일의 크기
+		long fileSize=myFile.getSize();
+		//저장할 파일의 이름을 Universal Unique 한 문자열로 얻어내기
+		String saveFileName=UUID.randomUUID().toString();
+		//저장할 파일의 전체 경로 구성하기
+		String filePath=fileLocation+File.separator+saveFileName;
+		try {
+			//업로드 파일을 저장할 파일 객체 생성
+			File savefile=new File(filePath);
+			myFile.transferTo(savefile);
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//원래는 DB에 저장해야 하지만 테스트를 위해 view page 에 전달해서 출력한다.
+		model.addAttribute("title",dto.getTitle());
+		model.addAttribute("orgFileName",orgFileName);
+		model.addAttribute("fileSize",fileSize);
+		model.addAttribute("saveFileName",saveFileName);
+		
+		return "file/upload";
+	}
+	
+	
+	@GetMapping("/file/new")
+	public String newForm() {
+		
+		return "file/new";
+	}
+}
